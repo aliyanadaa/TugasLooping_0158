f514fd32d4a3dd88cadb68d124218eb483b54ef1
aliyanadaa <aliyanadaq@gmail.com>
2026-03-31 12:49:33 +0700

    lengkapi logika cekPrima dengan while

--- tugas1.cpp
+++ tugas1.cpp
@@ -10,5 +10,14 @@ void input() {
 }
 
 bool cekPrima(int n) {
+    if (n <= 1) return false;
+
+    int i = 2;
+    while (i <= n / 2) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
     return true;
 }
